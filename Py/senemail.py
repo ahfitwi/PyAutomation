f92f302c519081960c91589d80927486f9023c57
Alem Haddush Fitwi <fitwialem2022@fb.com>
2022-12-19 02:56:16 -0500

    Create senemail.py

---
+++ Py/senemail.py
@@ -0,0 +1,30 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+contacts = [art@gmail.com', 'test@example.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'ART Update'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts[0]
+
+msg.set_content('This is a plain text email')
+
+msg.add_alternative("""\
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1 style="color:SlateGray;">ART was carried out today, yyyy-mm-dd, and test results are here.</h1>
+    </body>
+</html>
+""", subtype='html')
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
